3b943686189a9b6c98cc17a291fd4e058a6d5f41
Martin <martingramajo08@gmail.com>
2024-11-01 15:26:12 -0300

    tarea: hacer todos los todo dejado en el layout

---
+++ src/app/dashboard/rest-todos/page.tsx
@@ -0,0 +1,8 @@
+
+export default function RestTodosPage() {
+  return (
+    <div>
+      <h1>RestTodosPage Page</h1>
+    </div>
+  );
+}
---
+++ src/components/SidebarItem.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from "react";
+import { CiBookmarkCheck } from "react-icons/ci";
+
+
+
+interface Props {
+    path: string;
+    icon: JSX.Element;
+    title: string;
+  }
+
+
+const SidebarItem = ({ path, icon, title }: Props) => {
+  return (
+   
+      <Link
+        href={path}
+        className="relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-gradient-to-r from-sky-600 to-cyan-400"
+      >
+        {icon}
+        <span className="-mr-1 font-medium">{title}</span>
+      </Link>
+   
+  );
+};
+
+export default SidebarItem;
